9942afcd2a93e67294ae6b42a529d3542cdb78bb
Florian <bouclet.florian@hotmail.com>
2022-12-11 00:16:09 +0100

    banner and knife section

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,6 @@
 import bg from '../assets/bg.png';
 import chef from '../assets/chef.png';
-import G from '../assets/G.png';
+import Q from '../assets/Q.png';
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
 import gallery03 from '../assets/gallery03.png';
@@ -24,7 +24,7 @@ import quaiLogo from '../assets/quai-logo.png';
 export default {
   bg,
   chef,
-  G,
+  Q,
   gallery01,
   gallery02,
   gallery03,
